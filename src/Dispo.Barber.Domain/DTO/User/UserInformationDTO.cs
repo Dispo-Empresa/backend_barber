f79c6546daaa37dcaadb35703da56a4182a60bc5
guilhermelimaaDoAmaral <guilherme_l_amaral@estudante.sc.senai.br>
2024-10-09 11:12:54 -0300

    Adcionado Id User no UserInformationDTO conforme solicitado

--- src/Dispo.Barber.Domain/DTO/User/UserInformationDTO.cs
+++ src/Dispo.Barber.Domain/DTO/User/UserInformationDTO.cs
@@ -2,6 +2,7 @@
 {
     public class UserInformationDTO
     {
+        public long Id { get; set; }
         public string? Name { get; set; }
         public string? Surname { get; set; }
         public bool Active { get; set; } = true;
