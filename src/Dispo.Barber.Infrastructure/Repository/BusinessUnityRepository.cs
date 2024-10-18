3b8f389330f570177e82a8567b4f831848aab2a1
guilhermelimaaDoAmaral <guilherme_l_amaral@estudante.sc.senai.br>
2024-10-17 22:11:10 -0300

    commit

--- src/Dispo.Barber.Infrastructure/Repository/BusinessUnityRepository.cs
+++ src/Dispo.Barber.Infrastructure/Repository/BusinessUnityRepository.cs
@@ -14,12 +14,21 @@ namespace Dispo.Barber.Infrastructure.Repository
             this.context = context;
         }
 
-        public async Task<List<User>> GetUsersAsync(long id)
+
+        public async Task<List<User>> GetUsersAsync(CancellationToken cancellationToken, long id)
         {
             return await context.BusinessUnities.Where(w => w.Id == id)
                                 .Include(i => i.Users)
-                                .SelectMany(s => s.Users.Where(x => x.Active))
-                                .ToListAsync();
+                                .SelectMany(s => s.Users.Where(x => x.Status == UserStatus.Active))
+                                .ToListAsync(cancellationToken);
+        }
+
+        public async Task<List<User>> GetPendingUsersAsync(CancellationToken cancellationToken, long id)
+        {
+            return await context.BusinessUnities.Where(w => w.Id == id)
+                                .Include(i => i.Users)
+                                .SelectMany(s => s.Users.Where(x => x.Status == UserStatus.Pending))
+                                .ToListAsync(cancellationToken);
         }
 
         public async Task<long> GetIdByCompanyAsync(long companyId)
