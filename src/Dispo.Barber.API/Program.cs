5e88b4b3915b04f2e68332bde2c9787b22d7b2d2
ArtuoS <arthureduardomolinari@gmail.com>
2025-02-10 21:31:41 -0300

    Env para o OpenTelemetry.

--- src/Dispo.Barber.API/Program.cs
+++ src/Dispo.Barber.API/Program.cs
@@ -179,7 +179,7 @@ builder.Services.AddOpenTelemetry()
             .AddEntityFrameworkCoreInstrumentation(o => o.SetDbStatementForText = true)
             .AddOtlpExporter(opts =>
             {
-                opts.Endpoint = new Uri("http://172.19.234.142:4317"); // OTLP gRPC endpoint
+                opts.Endpoint = new Uri(Environment.GetEnvironmentVariable("OTLP_ENDPOINT") ?? "");
             });
     });
 
