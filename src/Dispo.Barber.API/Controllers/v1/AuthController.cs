7732e7622476aacb346f597ff140b0373d1bd624
Matheus Teixeira <matheustexeirablu@hotmail.com>
2025-02-17 23:23:03 -0300

    Remoção de método teste para envio de notificações para o app

--- src/Dispo.Barber.API/Controllers/v1/AuthController.cs
+++ src/Dispo.Barber.API/Controllers/v1/AuthController.cs
@@ -26,18 +26,5 @@ namespace Dispo.Barber.API.Controllers.v1
             var jwt = await authAppService.RefreshAuthenticationToken(cancellationToken, refreshToken, Request.Headers.Authorization);
             return Ok(jwt);
         }
-
-        [AllowAnonymous]
-        [HttpPost]
-        public async Task<IActionResult> LancarNotificacao(CancellationToken cancellationToken)
-        {
-            await notificationService.NotifyAsync(cancellationToken,
-                                                  "dVawZ8UZRzOU3ciGgrOLEX:APA91bGm7TXLvOVzTC6XsvjiH7naPXoFi29AcZ5JtqIVUgMKyLEzx4b7PKzpL67O9gGGFCAydacjwZEV0OwseEO7iToETiFHqZP2zYUIRtZbLHxufLwcK7Q",
-                                                  "Novo agendamento",
-                                                  "Matheus fez um novo agendamento para o dia 17/01",
-                                                  NotificationType.NewAppointment);
-
-            return Ok();
-        }
     }
 }
