7e9aa083c85467f11ecfa0ecc04ab403fa2b7442
Matheus Teixeira <matheustexeirablu@hotmail.com>
2025-03-06 22:58:23 -0300

    teste horario universal

--- src/Dispo.Barber.API/Controllers/v1/UserController.cs
+++ src/Dispo.Barber.API/Controllers/v1/UserController.cs
@@ -140,6 +140,7 @@ namespace Dispo.Barber.API.Controllers.v1
         {
             try
             {
+                requestDto.DateTimeSchedule = requestDto.DateTimeSchedule.ToUniversalTime();
                 var result = await informationChatService.GetAvailableSlotsAsync(cancellationToken, requestDto);
 
                 return Ok(result);
