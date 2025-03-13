e84b259acc93a2fd2ccde2584fd5876bc25c1e74
Matheus Teixeira <matheustexeirablu@hotmail.com>
2025-03-13 13:19:33 -0300

    Update do telefone opcional

--- src/Dispo.Barber.Domain/DTOs/User/UpdateUserDTO.cs
+++ src/Dispo.Barber.Domain/DTOs/User/UpdateUserDTO.cs
@@ -4,7 +4,7 @@
     {
         public string? Name { get; set; }
         public string? Surname { get; set; }
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
         public string? Password { get; set; }
         public string? DeviceToken { get; set; }
     }
