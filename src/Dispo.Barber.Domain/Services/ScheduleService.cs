9a210adafd5845b994a3976a3eb264f9933dd2a7
ArtuoS <arthureduardomolinari@gmail.com>
2025-04-01 21:42:42 -0300

    Ajustando schedule V1.

--- src/Dispo.Barber.Domain/Services/ScheduleService.cs
+++ src/Dispo.Barber.Domain/Services/ScheduleService.cs
@@ -4,6 +4,7 @@ using Dispo.Barber.Domain.Entities;
 using Dispo.Barber.Domain.Exceptions;
 using Dispo.Barber.Domain.Repositories;
 using Dispo.Barber.Domain.Services.Interface;
+using Google.Api.Gax;
 
 namespace Dispo.Barber.Domain.Services
 {
@@ -12,6 +13,9 @@ namespace Dispo.Barber.Domain.Services
         public async Task CreateAsync(CancellationToken cancellationToken, CreateScheduleDTO createScheduleDTO)
         {
             var schedule = mapper.Map<UserSchedule>(createScheduleDTO);
+
+            await ValidateSchedule(cancellationToken, schedule);
+
             await repository.AddAsync(cancellationToken, schedule);
             await repository.SaveChangesAsync(cancellationToken);
         }
@@ -69,6 +73,65 @@ namespace Dispo.Barber.Domain.Services
 
             repository.Update(schedule);
             await repository.SaveChangesAsync(cancellationToken);
+        }
+
+        private async Task ValidateSchedule(CancellationToken cancellationToken, UserSchedule schedule)
+        {
+            if (schedule.DayOff && schedule.StartDay > schedule.EndDay)
+            {
+                throw new BusinessException("O horário inicial não pode ser maior nem igual o horário final.");
+            }
+
+            if (!schedule.IsDatesValid())
+            {
+                await ValidateOverride(cancellationToken, schedule);
+            }
+
+            var (start, end) = schedule.ParseDates();
+            if (schedule.IsRest && (start > end || start == end))
+            {
+                throw new BusinessException("O horário inicial não pode ser maior nem igual o horário final.");
+            }
+
+            await ValidateOverride(cancellationToken, schedule);
+        }
+
+        private async Task ValidateOverride(CancellationToken cancellationToken, UserSchedule schedule)
+        {
+            var existingSchedules = await repository.GetAsync(cancellationToken, w => w.Enabled && (w.IsRest || w.DayOff) && w.UserId == schedule.UserId && w.DayOfWeek == schedule.DayOfWeek);
+            foreach (var existingSchedule in existingSchedules)
+            {
+                if (existingSchedule.DayOff)
+                {
+                    HandleDayOffOverride(schedule, existingSchedule);
+                    continue;
+                }
+
+                HandleRestOverride(schedule, existingSchedule);
+            }
+        }
+
+        private void HandleDayOffOverride(UserSchedule schedule, UserSchedule existingSchedule)
+        {
+            if (schedule.StartDay < existingSchedule.EndDay && schedule.EndDay > existingSchedule.StartDay)
+            {
+                throw new BusinessException("O horário novo sobrepõe um já existente.");
+            }
+        }
+
+        private void HandleRestOverride(UserSchedule schedule, UserSchedule existingSchedule)
+        {
+            if (!existingSchedule.IsDatesValid() || !schedule.IsDatesValid())
+            {
+                return;
+            }
+
+            var (newStart, newEnd) = schedule.ParseDates();
+            var (existingStart, existingEnd) = existingSchedule.ParseDates();
+            if (newStart < existingEnd && newEnd > existingStart)
+            {
+                throw new BusinessException("O horário novo sobrepõe um já existente.");
+            }
         }
     }
 }
