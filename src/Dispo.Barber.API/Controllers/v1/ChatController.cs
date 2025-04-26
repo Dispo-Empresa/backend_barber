b2259259093339c6318cc99e4dcd2e04bb906a4a
Matheus Teixeira <matheustexeirablu@hotmail.com>
2025-04-26 19:06:59 -0300

    Exclusão de endpoints para SMS no controller do chat

--- src/Dispo.Barber.API/Controllers/v1/ChatController.cs
+++ src/Dispo.Barber.API/Controllers/v1/ChatController.cs
@@ -48,49 +48,6 @@ namespace Dispo.Barber.API.Controllers.v1
             }
         }
 
-        [HttpPost("phone")]
-        public async Task<IActionResult> GenerateSmsCode([FromBody] string phone)
-        {
-            try
-            {
-                var codeRandom = new Random().Next(1000, 9999).ToString();
-
-                var fullMessageBody = $"Seu código de verificação é: {codeRandom}";
-
-                await smsService.SendMessageAsync(phone, fullMessageBody);
-
-                cache.Add(phone, codeRandom);
-
-                return Ok(new { message = "Código enviado com sucesso." });
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { message = $"Erro ao enviar SMS: {ex.Message}" });
-            }
-        }
-
-        [HttpGet("phone")]
-        public async Task<IActionResult> GetSmsCode([FromQuery] string phone, [FromQuery] string sms)
-        {
-            try
-            {
-                var smsInCache = cache.Get(phone);
-
-                // Excluir o token do cache caso já está válido
-
-                if (smsInCache == null)
-                {
-                    throw new Exception("O código expirou.");
-                }
-
-                return Ok(new { data = smsInCache == sms });
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new { message = $"Erro ao buscar o código do sms: {ex.Message}" });
-            }
-        }
-
         [HttpGet("users/{id}/information")]
         public async Task<IActionResult> GetUserInformationById(CancellationToken cancellationToken, long id)
         {
