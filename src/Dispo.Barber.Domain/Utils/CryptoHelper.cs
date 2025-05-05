46b0c36927d9eae24b5afbbc99d137b5fece2c83
matheus.amaral89y <matheus.amaral89y@gmail.com>
2025-05-05 17:56:59 -0300

    Ajuste para usar URL-SAFE

--- src/Dispo.Barber.Domain/Utils/CryptoHelper.cs
+++ src/Dispo.Barber.Domain/Utils/CryptoHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Security.Cryptography;
+﻿using System.Buffers.Text;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace Dispo.Barber.Domain.Utils
@@ -8,27 +9,37 @@ namespace Dispo.Barber.Domain.Utils
         private static readonly string Key = "DispoAuraApp-2025-SECURE-KEY@963";
         private static readonly string IV = "AuraInitVector20";
 
+        private static string ToBase64Url(string base64)
+        {
+            return base64.Replace("+", "-").Replace("/", "_").Replace("=", "");
+        }
+
+        private static string FromBase64Url(string base64Url)
+        {
+            string base64 = base64Url.Replace("-", "+").Replace("_", "/");
+            switch (base64.Length % 4)
+            {
+                case 2: base64 += "=="; break;
+                case 3: base64 += "="; break;
+            }
+            return base64;
+        }
+
         public static string Encrypt(string plainText)
         {
-            try
-            {
-                using Aes aesAlg = Aes.Create();
-                aesAlg.Key = Encoding.UTF8.GetBytes(Key);
-                aesAlg.IV = Encoding.UTF8.GetBytes(IV);
+            using Aes aesAlg = Aes.Create();
+            aesAlg.Key = Encoding.UTF8.GetBytes(Key);
+            aesAlg.IV = Encoding.UTF8.GetBytes(IV);
 
-                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
 
-                using MemoryStream ms = new();
-                using CryptoStream cs = new(ms, encryptor, CryptoStreamMode.Write);
-                using (StreamWriter sw = new(cs))
-                    sw.Write(plainText);
+            using MemoryStream ms = new();
+            using CryptoStream cs = new(ms, encryptor, CryptoStreamMode.Write);
+            using (StreamWriter sw = new(cs))
+                sw.Write(plainText);
 
-                return Convert.ToBase64String(ms.ToArray());
-            }
-            catch (Exception e)
-            {
-                throw e;
-            }   
+            var base64 = Convert.ToBase64String(ms.ToArray());
+            return ToBase64Url(base64); 
         }
 
         public static string Decrypt(string cipherText)
@@ -39,7 +50,7 @@ namespace Dispo.Barber.Domain.Utils
 
             ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
 
-            byte[] buffer = Convert.FromBase64String(cipherText);
+            byte[] buffer = Convert.FromBase64String(FromBase64Url(cipherText));
 
             using MemoryStream ms = new(buffer);
             using CryptoStream cs = new(ms, decryptor, CryptoStreamMode.Read);
