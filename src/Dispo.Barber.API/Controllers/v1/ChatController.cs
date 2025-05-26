a2ee95506725ce012f2793fd5b338969bba4adf7
matheus.amaral89y <matheus.amaral89y@gmail.com>
2025-05-26 18:37:57 -0300

    Ajuste chat controller

--- src/Dispo.Barber.API/Controllers/v1/ChatController.cs
+++ src/Dispo.Barber.API/Controllers/v1/ChatController.cs
@@ -1,6 +1,7 @@
 ﻿using Dispo.Barber.Application.AppServices.Interface;
 using Dispo.Barber.Domain.DTOs.Appointment;
 using Dispo.Barber.Domain.DTOs.Chat;
+using Dispo.Barber.Domain.Entities;
 using Dispo.Barber.Domain.Services.Interface;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -127,22 +128,43 @@ namespace Dispo.Barber.API.Controllers.v1
         [HttpPatch("appointments/{id}/cancel")]
         public async Task<IActionResult> CancelAppointment(CancellationToken cancellationToken, [FromRoute] long id)
         {
-            await appointmentAppService.CancelAppointmentAsync(cancellationToken, id, notifyUsers: true);
-            return Ok();
+            try
+            {
+                await appointmentAppService.CancelAppointmentAsync(cancellationToken, id, notifyUsers: true);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Ocorreu um erro ao cancelar o agendamento", error = ex.Message });
+            }           
         }
 
         [HttpPatch("appointments/{idAppointment}/information-appointment")]
         public async Task<IActionResult> GetInformationAppointmentById(CancellationToken cancellationToken, [FromRoute] long idAppointment)
         {
-            var result = await informationChatService.GetInformationAppointmentChatByIdAppointment(cancellationToken, idAppointment);
-            return Ok(result);
+            try
+            {
+                var result = await informationChatService.GetInformationAppointmentChatByIdAppointment(cancellationToken, idAppointment);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Ocorreu um erro ao buscar informações do agendamento", error = ex.Message });
+            }          
         }
 
         [HttpPost("appointments/reschedule")]
         public async Task<IActionResult> Reschedule(CancellationToken cancellationToken, [FromBody] CreateAppointmentDTO createAppointmentDTO)
         {
-            await appointmentAppService.RescheduleAsync(cancellationToken, createAppointmentDTO);
-            return Ok();
+            try
+            {
+                await appointmentAppService.RescheduleAsync(cancellationToken, createAppointmentDTO);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = "Ocorreu um erro ao reagendar", error = ex.Message });
+            }
         }
     }
 }
